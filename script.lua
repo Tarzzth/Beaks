5ef9caf72ff2b7594a770cc958da48d4bd7a1d17
Tarzzth <tarzzth7@gmail.com>
2025-05-18 07:50:51 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,9 @@
+
+-- Load Obf Main Script /
 print('Load Script Flux Hub')
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Tarzzth/Beaks/main/Obf.lua'))()
+
+-- Load Webhhok Discord /
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Tarzzth/Beaks/main/webhook.lua'))()
 warn('Send Webhook Suscess')
--- Load Obf Main Script
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Tarzzth/Beaks/main/Obf.lua'))()
